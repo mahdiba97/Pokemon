187c640b1e376649a7b3a6c3c56a943f2249add9
Mahdi Bahmanpour <mahdi.bahmanpour97@gmail.com>
2021-10-01 17:03:48 +0330

    Add provider functions to appModule

--- app/src/main/java/com/mahdiba97/pokemon/di/AppModule.kt
+++ app/src/main/java/com/mahdiba97/pokemon/di/AppModule.kt
@@ -1,10 +1,28 @@
 package com.mahdiba97.pokemon.di
 
+import com.mahdiba97.pokemon.Constants.BASE_URL
+import com.mahdiba97.pokemon.data.remote.PokeService
+import com.mahdiba97.pokemon.repository.PokemonRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun providePokemonRepository(service: PokeService) = PokemonRepository(service)
+
+    @Singleton
+    @Provides
+    fun providePokeService(): PokeService {
+        return Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL).build().create(PokeService::class.java)
+    }
 }
