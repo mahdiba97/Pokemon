cd319318487928af1d5f19b8ca771ac6c03dedba
Mahdi Bahmanpour <mahdi.bahmanpour97@gmail.com>
2021-10-02 20:26:05 +0330

    Add new color to colors,override a few default colors

--- app/src/main/java/com/mahdiba97/pokemon/ui/theme/Color.kt
+++ app/src/main/java/com/mahdiba97/pokemon/ui/theme/Color.kt
@@ -5,4 +5,12 @@ import androidx.compose.ui.graphics.Color
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+
+val yellow200 = Color(0xFFFFF59D)
+val yellow500 = Color(0xFFFFEB3B)
+val yellow700 = Color(0xFFFBC02D)
+
+val blue200 = Color(0xFF90CAF9)
+val blue500 = Color(0xFF2196F3)
+val blue700 = Color(0xFF1976D2)
--- app/src/main/java/com/mahdiba97/pokemon/ui/theme/Theme.kt
+++ app/src/main/java/com/mahdiba97/pokemon/ui/theme/Theme.kt
@@ -5,17 +5,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = yellow500,
+    primaryVariant = yellow700,
+    secondary = blue200,
+    onBackground = Color.White,
+    background = Color(0xff101010),
+    surface = Color(0xff303030),
+    onSurface = Color.White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = blue500,
+    primaryVariant = yellow700,
+    secondary = blue200,
+    onBackground = Color.Black,
+    background = Color.Cyan,
+    surface = Color.White,
+    onSurface = Color.White
 
     /* Other default colors to override
     background = Color.White,
@@ -39,6 +48,6 @@ fun PokemonTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composabl
         colors = colors,
         typography = Typography,
         shapes = Shapes,
-        content = content
+        content = content,
     )
 }
